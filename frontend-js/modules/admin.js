f393919180a36bd7d2ba77f0f54a5b72d21f136a
Kevin Jordan <glasskdj@MacDev.local>
2019-09-11 07:31:42 -0700

    new branch attempt

--- frontend-js/modules/admin.js
+++ frontend-js/modules/admin.js
@@ -9,7 +9,6 @@ export default class Admin {
        this.navTitles.forEach(el => {
            el.addEventListener('click', () => {
                 this.subMenuToggle(el);
-                
            })
        })
     }
@@ -17,21 +16,13 @@ export default class Admin {
     //methods
 
     subMenuToggle(el) {
-        el.nextElementSibling.classList.toggle('show');
+        
+        Array.from(el.children)[1].classList.toggle('show');
         el.querySelector('.carrot').classList.toggle('rotate');
         
-        // console.log(document.querySelectorAll('.sidenav__dropdown'))
-        
-        document.querySelectorAll('.sidenav__title').forEach((menu) => {
-            // console.log(el.id);
-            // console.log(menu.id);
-            if(el.id != menu.id) {
-                console.log(menu.classList);
-                console.log(menu.querySelector('.sidenav__dropdown'))
-                
-            }   
-                
-            
+        document.querySelectorAll('.sidenav__dropdown').forEach((menu) => {
+           console.log('------');
+           console.log(menu.classList.contains('.show'));
         })  
     }
     
